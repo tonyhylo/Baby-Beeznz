042bdd7638483103f4f7a60bd9d03baf3d6a77a8
Tony Lo <tonylo@Anureets-MacBook-Air.local>
2023-02-01 20:57:55 -0500

    Added oauth

--- models/baby.js
+++ models/baby.js
@@ -3,19 +3,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const feedSchema = new Schema({
-  date: { type: Date },
-  quantity: { type: Number, min: 0 },
+  date: { type: Date, required: true },
+  quantity: { type: Number, required: true, min: 0 },
   duration: { type: Number, min: 0 },
 });
 
 const bmSchema = new Schema({
-  date: { type: Date },
-  bm: { type: String, enum: ["Wet", "Mix"] },
+  date: { type: Date, required: true },
+  bm: { type: String, required: true, enum: ["Wet", "Mix"] },
 });
 
 const babySchema = new Schema({
-  name: { type: String },
-  dob: { type: Date },
+  name: { type: String, required: true },
+  dob: { type: Date, required: true },
   feeds: [feedSchema],
   bms: [bmSchema]
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
+const passport = require('passport');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    // prompt: "select_account"
+  }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/babies',
+    failureRedirect: '/babies'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/babies');
+  });
+});
+
 
 module.exports = router;
